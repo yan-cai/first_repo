4f8b0aab2930af84da182547254a246be0ab59cf
yan-cai <cai_yan77@yahoo.com>
2026-06-18 17:14:26 -0700

    Add main.cpp with Hello World program

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print a message to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
